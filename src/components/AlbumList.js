7ca53cf351bc59c4081f84067ab9d01f7878a5aa
Nygard-R1 <thomas.sickert@gmail.com>
2017-03-01 07:57:43 -0600

    Get Request using Axiom
    - Added GET request using Axiom

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'; // React.createElement is called from
                                           // JSX, so don't remove this
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
   componentWillMount() {
-    console.log('componentWillMount in AlbumList');
-    debugger; //debug breakpoint
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => console.log(response));
   }
 
   render() {
